18ab7bd19412cfd836005a89d82587d36c686711
Hiroshi Miura <miurahr@linux.com>
2022-06-30 16:29:58 +0900

    wip
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/java/tokyo/northside/omegat/theme/AdaptiveTheme.java
+++ src/main/java/tokyo/northside/omegat/theme/AdaptiveTheme.java
@@ -24,13 +24,13 @@
 package tokyo.northside.omegat.theme;
 
 
+import com.github.weisj.darklaf.DarkLaf;
 import com.github.weisj.darklaf.platform.ThemePreferencesHandler;
 import com.github.weisj.darklaf.theme.Theme;
 import com.github.weisj.darklaf.theme.info.ColorToneRule;
 import com.github.weisj.darklaf.theme.info.ContrastRule;
 import com.github.weisj.darklaf.theme.info.PreferredThemeStyle;
 import com.github.weisj.darklaf.theme.info.ThemeProvider;
-import com.github.weisj.darklaf.theme.laf.SynthesisedThemedLaf;
 import org.omegat.gui.theme.DefaultFlatTheme;
 
 import javax.swing.UIDefaults;
@@ -47,11 +47,11 @@ public class AdaptiveTheme extends BasicLookAndFeel {
     private static final String DESCRIPTION = "Adaptive theme based on DarkLaf";
 
     private static Theme baseTheme;
-    private static SynthesisedThemedLaf baseSynthesisedThemeLaf;
+    private static DarkLaf laf;
 
     public AdaptiveTheme() {
         baseTheme = themeForPreferredStyle(getPreferredThemeStyle());
-        baseSynthesisedThemeLaf = new SynthesisedThemedLaf(baseTheme);
+        laf = new DarkLaf();
     }
     private static ThemeProvider themeProvider;
 
@@ -100,6 +100,7 @@ public class AdaptiveTheme extends BasicLookAndFeel {
     public static Theme themeForPreferredStyle(final PreferredThemeStyle style) {
         return getThemeProvider().getTheme(style);
     }
+
     /**
      * Adjust a color by adding some constant to its RGB values, clamping to the
      * range 0-255.
@@ -112,15 +113,13 @@ public class AdaptiveTheme extends BasicLookAndFeel {
     }
 
     public UIDefaults getDefaults() {
-        UIDefaults defaults = baseSynthesisedThemeLaf.getDefaults();
-
-        // get OmegaT defaults
-        defaults = DefaultFlatTheme.setDefaults(defaults, ID);
-
         boolean dark = baseTheme.getColorToneRule() == ColorToneRule.DARK;
-        boolean highContrast = baseTheme.getContrastRule() == ContrastRule.HIGH_CONTRAST;
-
-        return defaults;
+        // boolean highContrast = baseTheme.getContrastRule() == ContrastRule.HIGH_CONTRAST;
+        if (dark) {
+            return getDarkDefaults();
+        } else {
+            return getLightDefaults();
+        }
     }
 
     public String getID() {
@@ -143,6 +142,204 @@ public class AdaptiveTheme extends BasicLookAndFeel {
     @Override
     public boolean isSupportedLookAndFeel() {
         return true;
+    }
+
+    private UIDefaults getLightDefaults() {
+        UIDefaults defaults = super.getDefaults();
+        defaults = DefaultFlatTheme.setDefaults(defaults, ID);
+
+        Color standardBgColor = defaults.getColor("Panel.background");
+        defaults.put("TextPane.background", Color.WHITE);
+
+        // Borders
+        Color borderColor = new Color(0x0f5e9c);
+        defaults.put("OmegaTBorder.color", borderColor);
+        defaults.put("OmegaTDockablePanel.border", new MatteBorder(1, 1, 1, 1, borderColor));
+
+        // OmegaT-defined Dockables.
+        defaults.put("OmegaTDockablePanel.border", new MatteBorder(1, 1, 1, 1, borderColor));
+        defaults.put("OmegaTDockablePanel.isProportionalMargins", true);
+
+        // Default application Colors
+        defaults.put("OmegaT.activeSource", new Color(0xc0ffc0));
+        defaults.put("OmegaT.source", new Color(0xc0ffc0));
+        defaults.put("OmegaT.noted", new Color(0xc0ffff));
+        defaults.put("OmegaT.untranslated", new Color(0xc0c0ff));
+        defaults.put("OmegaT.translated", new Color(0xffff99));
+        defaults.put("OmegaT.nonUnique", new Color(0x808080));
+        defaults.put("OmegaT.placeholder", new Color(0x969696));
+        defaults.put("OmegaT.removeTextTarget", new Color(0xff0000));
+        defaults.put("OmegaT.nbsp", new Color(0xc8c8c8));
+        defaults.put("OmegaT.whiteSpace", new Color(0x808080));
+        defaults.put("OmegaT.bidiMarkers", new Color(0xc80000));
+        defaults.put("OmegaT.paragraphStart", new Color(0xaeaeae));
+        defaults.put("OmegaT.markComesFromTm", new Color(0xfa8072));
+        defaults.put("OmegaT.markComesFromTxXice", new Color(0xaf76df));
+        defaults.put("OmegaT.markComesFromTmX100pc", new Color(0xff9408));
+        defaults.put("OmegaT.markComesFromTmXauto", new Color(0xff9596));
+        defaults.put("OmegaT.markComesFromTmXendorced", new Color(0xffccff));
+        defaults.put("OmegaT.replace", new Color(0x0000ff));
+        defaults.put("OmegaT.languageTools", new Color(0x0000ff));
+        defaults.put("OmegaT.transTips", new Color(0x0000ff));
+        defaults.put("OmegaT.spellCheck", new Color(0xff0000));
+        defaults.put("OmegaT.terminology", new Color(0xff6600));
+        defaults.put("OmegaT.matchesChanged", new Color(0x0000ff));
+        defaults.put("OmegaT.matchesUnchanged", new Color(0x00ff00));
+        defaults.put("OmegaT.matchesInsActive", new Color(0x0000ff));
+        defaults.put("OmegaT.matchesInsInactive", new Color(0x808080));
+        defaults.put("OmegaT.hyperlink", new Color(0x0000ff));
+        defaults.put("OmegaT.searchFoundMark", new Color(0x0000ff));
+        defaults.put("OmegaT.searchReplaceMark", new Color(0xff9900));
+        defaults.put("OmegaT.notificationMin", new Color(0xfff2d4));
+        defaults.put("OmegaT.notificationMax", new Color(0xff9900));
+        defaults.put("OmegaT.alignerAccepted", new Color(0x15bb45));
+        defaults.put("OmegaT.alignerNeedsReview", new Color(0xff0000));
+        defaults.put("OmegaT.alignerHighlight", new Color(0xffff00));
+        defaults.put("OmegaT.alignerTableRowHighlight", new Color(0xc8c8c8));
+        defaults.put("OmegaT.projectFilesCurrentFileForeground", new Color(0x0));
+        defaults.put("OmegaT.projectFilesCurrentFileBackground", new Color(0xc8ddf2));
+        defaults.put("OmegaT.searchFieldErrorText", new Color(0xff0000));
+        defaults.put("OmegaT.searchDimmedBackground", new Color(0x80, 0x80, 0x80, 0x80));
+        defaults.put("OmegaT.searchResultBorder", new Color(0xEE, 0xD2, 0x00, 0x80));
+        defaults.put("OmegaT.machinetranslateSelectedHighlight", new Color(0xffff00));
+
+        // FIXME: VLDocking values have to be set to the "developer defaults"
+        // not the "LAF defaults" because that's where
+        // DockingUISettings#installUI puts them
+        UIManager.put("DockViewTitleBar.border", new MatteBorder(1, 1, 1, 1, borderColor));
+
+        int cornerRadius = 0;
+
+        // Panel title bars
+        Color activeTitleText = defaults.getColor("Label.foreground");
+        Color activeTitleBgColor = adjustRGB(standardBgColor, 0xF6 - 0xEE);
+        Color inactiveTitleText = new Color(0x808080);
+        defaults.put("InternalFrame.activeTitleForeground", activeTitleText);
+        defaults.put("InternalFrame.activeTitleBackground", activeTitleBgColor);
+        defaults.put("InternalFrame.inactiveTitleForeground", inactiveTitleText);
+        defaults.put("InternalFrame.inactiveTitleBackground", standardBgColor);
+
+        // Undocked panel
+        defaults.put("activeCaption", Color.WHITE);
+        defaults.put("activeCaptionBorder", borderColor);
+        defaults.put("inactiveCaption", standardBgColor);
+        defaults.put("inactiveCaptionBorder", borderColor);
+
+        // Standard components customize
+        defaults.put("Component.arrowType", "triangle");
+        defaults.put("Button.arc", 999);
+        defaults.put("Component.arc", 999);
+        defaults.put("CheckBox.arc", 999);
+        defaults.put("ProgressBar.arc", 999);
+        defaults.put("TextComponent.arc", 999);
+        defaults.put("ScrollBar.trackArc", 999);
+        defaults.put("ScrollBar.thumbArc", 999);
+        defaults.put("ScrollBar.trackInsets", new Insets(2, 4, 2, 4));
+        defaults.put("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+        defaults.put("ScrollBar.track", new Color(0xe0e0e0));
+        defaults.put("TabbedPane.tabSeparatorsFullHeight", true);
+        defaults.put("TabbedPane.selectedBackground", Color.white);
+
+        return defaults;
+    }
+
+    private UIDefaults getDarkDefaults() {
+        UIDefaults defaults = super.getDefaults();
+
+        // get OmegaT defaults
+        defaults = DefaultFlatTheme.setDefaults(defaults, ID);
+
+        // GTK+ has bug that TextPane background is fixed white.
+        // https://sourceforge.net/p/omegat/bugs/1013/
+        Color standardBgColor = defaults.getColor("Panel.background");
+        defaults.put("TextPane.background", standardBgColor);
+
+        // Borders
+        Color borderColor = new Color(0x8aa5b8);
+        defaults.put("OmegaTBorder.color", borderColor);
+        defaults.put("OmegaTDockablePanel.border", new MatteBorder(1, 1, 1, 1, borderColor));
+
+        // Default application Colors
+        defaults.put("OmegaT.activeSource", new Color(0x287128));
+        defaults.put("OmegaT.source", new Color(0x82b682));
+        defaults.put("OmegaT.noted", new Color(0xa0c0c0));
+        defaults.put("OmegaT.untranslated", new Color(0x4d4daa));
+        defaults.put("OmegaT.translated", new Color(0x57572d));
+        defaults.put("OmegaT.nonUnique", new Color(0x808080));
+        defaults.put("OmegaT.placeholder", new Color(0x969696));
+        defaults.put("OmegaT.removeTextTarget", new Color(0xff0000));
+        defaults.put("OmegaT.nbsp", new Color(0xc8c8c8));
+        defaults.put("OmegaT.whiteSpace", new Color(0x808080));
+        defaults.put("OmegaT.bidiMarkers", new Color(0x480000));
+        defaults.put("OmegaT.paragraphStart", new Color(0xaeaeae));
+        defaults.put("OmegaT.markComesFromTm", new Color(0xaa8072));
+        defaults.put("OmegaT.markComesFromTxXice", new Color(0xaf76df));
+        defaults.put("OmegaT.markComesFromTmX100pc", new Color(0x3f54f8));
+        defaults.put("OmegaT.markComesFromTmXauto", new Color(0x3f2546));
+        defaults.put("OmegaT.markComesFromTmXendorced", new Color(0x005500));
+        defaults.put("OmegaT.replace", new Color(0x0000ff));
+        defaults.put("OmegaT.languageTools", new Color(0x0000ff));
+        defaults.put("OmegaT.transTips", new Color(0x0000ff));
+        defaults.put("OmegaT.spellCheck", new Color(0xff0000));
+        defaults.put("OmegaT.terminology", new Color(0xffa500));
+        defaults.put("OmegaT.matchesChanged", new Color(0x0000ff));
+        defaults.put("OmegaT.matchesUnchanged", new Color(0x00ff00));
+        defaults.put("OmegaT.matchesInsActive", new Color(0x0000ff));
+        defaults.put("OmegaT.matchesInsInactive", new Color(0x808080));
+        defaults.put("OmegaT.hyperlink", new Color(0x0000ff));
+        defaults.put("OmegaT.searchFoundMark", new Color(0x0000ff));
+        defaults.put("OmegaT.searchReplaceMark", new Color(0xff9900));
+        defaults.put("OmegaT.notificationMin", new Color(0x332233));
+        defaults.put("OmegaT.notificationMax", new Color(0x647354));
+        defaults.put("OmegaT.alignerAccepted", new Color(0x15bb45));
+        defaults.put("OmegaT.alignerNeedsReview", new Color(0xff0000));
+        defaults.put("OmegaT.alignerHighlight", new Color(0xffff00));
+        defaults.put("OmegaT.alignerTableRowHighlight", new Color(0xc8c8c8));
+        defaults.put("OmegaT.projectFilesCurrentFileForeground", new Color(0x0));
+        defaults.put("OmegaT.projectFilesCurrentFileBackground", new Color(0xc8ddf2));
+        defaults.put("OmegaT.searchFieldErrorText", new Color(0x7f0000));
+        defaults.put("OmegaT.searchDimmedBackground", new Color(0x80, 0x80, 0x80, 0x80));
+        defaults.put("OmegaT.searchResultBorder", new Color(0xEE, 0xD2, 0x00, 0x80));
+        defaults.put("OmegaT.machinetranslateSelectedHighlight", new Color(0xff9900));
+
+        // FIXME: VLDocking values have to be set to the "developer defaults"
+        // not the "LAF defaults" because that's where
+        // DockingUISettings#installUI puts them
+        UIManager.put("DockViewTitleBar.border", new MatteBorder(1, 1, 1, 1, borderColor));
+
+        // Panel title bars
+        Color activeTitleBgColor = adjustRGB(standardBgColor, 0xF6 - 0xEE);
+        Color activeTitleText = defaults.getColor("Label.foreground");
+        Color inactiveTitleText = new Color(0x767676);
+
+        defaults.put("InternalFrame.activeTitleForeground", activeTitleText);
+        defaults.put("InternalFrame.activeTitleBackground", activeTitleBgColor);
+        defaults.put("InternalFrame.inactiveTitleForeground", inactiveTitleText);
+        defaults.put("InternalFrame.inactiveTitleBackground", standardBgColor);
+
+        // Undocked panel
+        defaults.put("activeCaption", Color.GRAY);
+        defaults.put("activeCaptionBorder", borderColor);
+        defaults.put("inactiveCaption", standardBgColor);
+        defaults.put("inactiveCaptionBorder", borderColor);
+
+        // Standard components customize
+        defaults.put("Component.arrowType", "triangle");
+        defaults.put("Button.arc", 999);
+        defaults.put("Component.arc", 999);
+        defaults.put("CheckBox.arc", 999);
+        defaults.put("ProgressBar.arc", 999);
+        defaults.put("TextComponent.arc", 999);
+        defaults.put("ScrollBar.trackArc", 999);
+        defaults.put("ScrollBar.thumbArc", 999);
+        defaults.put("ScrollBar.trackInsets", new Insets(3, 5, 3, 5));
+        defaults.put("ScrollBar.thumbInsets", new Insets(3, 3, 3, 3));
+        defaults.put("ScrollBar.width", 16);
+        defaults.put("ScrollBar.track", new Color(0x757575));
+        defaults.put("TabbedPane.tabSeparatorsFullHeight", true);
+        defaults.put("TabbedPane.selectedBackground", Color.GRAY);
+
+        return defaults;
     }
 }
 
